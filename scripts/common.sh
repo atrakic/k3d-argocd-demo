e57ee662ffa37b82d5ce29cd9b4613d545320797
Admir Trakic <atrakic@users.noreply.github.com>
2021-07-27 20:19:57 +0200

    Ensure argocd permissions

--- scripts/common.sh
+++ scripts/common.sh
@@ -5,7 +5,8 @@ command -v $CMD >/dev/null 2>&1 || curl -s https://raw.githubusercontent.com/ran
 
 CMD=argocd
 command -v $CMD >/dev/null 2>&1 || curl -sSL -o /usr/local/bin/argocd \
-  https://github.com/argoproj/argo-cd/releases/download/"$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')"/argocd-linux-amd64
+  https://github.com/argoproj/argo-cd/releases/download/"$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')"/argocd-linux-amd64 \
+  chmod +x /usr/local/bin/argocd
 
 CMD=helm
 command -v $CMD >/dev/null 2>&1 || curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
