4c61aff9e2ce8a6b663d08a140ca48922c7da963
Admir Trakic <atrakic@users.noreply.github.com>
2021-07-27 20:13:28 +0200

    Install helm

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This repo shows how to install [Argo CD](https://argoproj.io/) and use it with [k3d](https://github.com/rancher/k3d)
 
+## Includes
+- Traefik v2
+
 ## Demo
 - [Argo CD login](https://argocd.k3d.local:8080/)
 - [GuestBook app](http://guestbook.localhost:8080/)
--- scripts/common.sh
+++ scripts/common.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-cluster_id=${cluster_id:-argocd}
-export cluster_id
+CMD=k3s
+command -v $CMD >/dev/null 2>&1 || curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
 
-command -v k3d >/dev/null 2>&1  || curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+CMD=argocd
+command -v $CMD >/dev/null 2>&1 || curl -sSL -o /usr/local/bin/argocd \
+  https://github.com/argoproj/argo-cd/releases/download/"$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')"/argocd-linux-amd64
 
-ARGO_VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
-command -v argocd >/dev/null 2>&1  || curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/"$ARGO_VERSION"/argocd-linux-amd64
+CMD=helm
+command -v $CMD >/dev/null 2>&1 || curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
 PATH=/usr/local/bin:$PATH
 export PATH
+
+cluster_id=${cluster_id:-argocd}
+export cluster_id
